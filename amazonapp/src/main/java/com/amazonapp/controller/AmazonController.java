bb04c1b35f83e4cf2a9bfc6d22e692ffd9410a06
RajeshUpadhyay <RajeshUpadhyay@Rev-PG02REP7>
2024-09-17 14:42:39 +0530

    sdxv

--- amazonapp/src/main/java/com/amazonapp/controller/AmazonController.java
+++ amazonapp/src/main/java/com/amazonapp/controller/AmazonController.java
@@ -1,8 +1,11 @@
 package com.amazonapp.controller;
 
 import java.util.Arrays;
+import java.util.List;
 
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.cloud.client.ServiceInstance;
+import org.springframework.cloud.client.discovery.DiscoveryClient;
 import org.springframework.http.HttpEntity;
 import org.springframework.http.HttpHeaders;
 import org.springframework.http.HttpMethod;
@@ -15,23 +18,40 @@ import org.springframework.web.servlet.ModelAndView;
 
 @Controller
 public class AmazonController {
+	//@Autowired
+	//RestTemplate restTemplate;
+	
 	@Autowired
-	RestTemplate restTemplate;
-
+	private DiscoveryClient discoveryClient;
+	
 	@RequestMapping("displayall12")
 	public ModelAndView displayAllProduct() {
-
 		
+		List<ServiceInstance> instances=discoveryClient.getInstances("BUYERSERVICE");
+		ServiceInstance serviceInstance=instances.get(0);
+		
+		String baseUrl=serviceInstance.getUri().toString(); //return http://localhost:8080
+		
+		baseUrl=baseUrl+"/displayAll";
+		
+		
+
+		RestTemplate restTemplate=new RestTemplate();
 
 		HttpHeaders headers = new HttpHeaders();
 		headers.setAccept(Arrays.asList(MediaType.APPLICATION_JSON));
 
 		HttpEntity<String> entity = new HttpEntity<String>(headers);
 
-		String str = restTemplate.exchange("http://localhost:8080/displayAll", HttpMethod.GET, entity, String.class)
+		String str = restTemplate.exchange(baseUrl, HttpMethod.GET, entity, String.class)
 				.getBody();
 
 		System.out.println(str);
+		
+		String str1=restTemplate.getForObject(
+				baseUrl,
+	            String.class);
+		System.out.println(str1);
 
 		ModelAndView mv = new ModelAndView();
 		return mv;
