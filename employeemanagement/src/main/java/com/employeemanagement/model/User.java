ec556f270fdde417da6ea2406db95003ceb9a591
RajeshUpadhyay <RajeshUpadhyay@Rev-PG02REP7>
2024-10-24 12:58:12 +0530

    hfgtdfd

---
+++ employeemanagement/src/main/java/com/employeemanagement/model/User.java
@@ -0,0 +1,57 @@
+package com.employeemanagement.model;
+
+/**
+ * User.java
+ * This is a model class represents a User entity
+ * @author Rajesh Upadhyay
+ *
+ */
+public class User {
+	protected int id;
+	protected String name;
+	protected String email;
+	protected String country;
+	
+	public User() {
+	}
+	
+	public User(String name, String email, String country) {
+		super();
+		this.name = name;
+		this.email = email;
+		this.country = country;
+	}
+
+	public User(int id, String name, String email, String country) {
+		super();
+		this.id = id;
+		this.name = name;
+		this.email = email;
+		this.country = country;
+	}
+
+	public int getId() {
+		return id;
+	}
+	public void setId(int id) {
+		this.id = id;
+	}
+	public String getName() {
+		return name;
+	}
+	public void setName(String name) {
+		this.name = name;
+	}
+	public String getEmail() {
+		return email;
+	}
+	public void setEmail(String email) {
+		this.email = email;
+	}
+	public String getCountry() {
+		return country;
+	}
+	public void setCountry(String country) {
+		this.country = country;
+	}
+}
